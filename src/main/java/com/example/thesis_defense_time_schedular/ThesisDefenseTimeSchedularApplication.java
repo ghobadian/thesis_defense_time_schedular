e434a28a650601424312ce0e6a3d4382670d6b38
koosha <kooshaghobadian@gmail.com>
2025-03-22 15:13:28 +0330

    initial

---
+++ src/main/java/com/example/thesis_defense_time_schedular/ThesisDefenseTimeSchedularApplication.java
@@ -0,0 +1,13 @@
+package com.example.thesis_defense_time_schedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisDefenseTimeSchedularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisDefenseTimeSchedularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thesis_defense_time_schedular/ThesisDefenseTimeSchedularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thesis_defense_time_schedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisDefenseTimeSchedularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
