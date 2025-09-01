acbf78188f70c5dac4f448da01b4b116683f8c35
koosha <kooshaghobadian@gmail.com>
2025-09-01 20:46:09 +0330

    add misc

--- src/main/java/ir/kghobad/thesis_defense_time_schedular/ThesisDefenseTimeSchedularApplication.java
+++ src/main/java/ir/kghobad/thesis_defense_time_schedular/ThesisDefenseTimeSchedularApplication.java
@@ -1,4 +1,4 @@
-package com.example.thesis_defense_time_schedular;
+package ir.kghobad.thesis_defense_time_schedular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
