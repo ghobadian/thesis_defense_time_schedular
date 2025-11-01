8075e75877a404f749d176acd146f36b2b541b62
koosha <kooshaghobadian@gmail.com>
2025-11-01 19:23:50 +0330

    complete student endpoints

--- src/main/java/ir/kghobad/thesis_defense_time_schedular/dao/StudentRepository.java
+++ src/main/java/ir/kghobad/thesis_defense_time_schedular/dao/StudentRepository.java
@@ -4,6 +4,10 @@ import ir.kghobad.thesis_defense_time_schedular.model.entity.user.student.Studen
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
+    Optional<Student> findByPhoneNumber(String phoneNumber);
 }
