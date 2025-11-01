feff9e79cecfbd06c83d4ee4018d1b46087e6899
koosha <kooshaghobadian@gmail.com>
2025-11-01 19:24:18 +0330

    complete professor endpoints

--- src/main/java/ir/kghobad/thesis_defense_time_schedular/controller/ProfessorController.java
+++ src/main/java/ir/kghobad/thesis_defense_time_schedular/controller/ProfessorController.java
@@ -1,10 +1,11 @@
 package ir.kghobad.thesis_defense_time_schedular.controller;
 
-import ir.kghobad.thesis_defense_time_schedular.model.dto.AuthResponseDTO;
-import ir.kghobad.thesis_defense_time_schedular.model.dto.LoginDTO;
-import ir.kghobad.thesis_defense_time_schedular.model.dto.TimeSlotDTO;
+import ir.kghobad.thesis_defense_time_schedular.model.dto.AvailableTimeInputDTO;
+import ir.kghobad.thesis_defense_time_schedular.model.dto.FormSuggestionInputDTO;
+import ir.kghobad.thesis_defense_time_schedular.model.dto.MeetingCompletionInputDTO;
+import ir.kghobad.thesis_defense_time_schedular.model.dto.ThesisFormOutputDTO;
+import ir.kghobad.thesis_defense_time_schedular.security.JwtUtil;
 import ir.kghobad.thesis_defense_time_schedular.service.ProfessorService;
-import ir.kghobad.thesis_defense_time_schedular.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,46 +14,66 @@ import java.util.List;
 @RestController
 @RequestMapping("/professor")
 public class ProfessorController {
-    private final ProfessorService professorService;
-    private final UserService userService;
-    
-    public ProfessorController(ProfessorService professorService, UserService userService) {
-        this.professorService = professorService;
-        this.userService = userService;
+    private final ProfessorService service;
+    private final JwtUtil jwtUtil;
+
+    public ProfessorController(ProfessorService service,
+                               JwtUtil jwtUtil) {
+        this.service = service;
+        this.jwtUtil = jwtUtil;
     }
-    
-    @PostMapping("/login")
-    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO loginDTO) {
-        return ResponseEntity.ok(userService.login(loginDTO));
+
+    @GetMapping("/forms")
+    public ResponseEntity<List<ThesisFormOutputDTO>> getThesisForms() {
+        return ResponseEntity.ok(service.getThesisForms());
     }
-    
-    @PostMapping("/{id}/accept-form/{formId}")
-    public ResponseEntity<?> acceptForm(@PathVariable Long id, @PathVariable Long formId) {
-        professorService.acceptForm(id, formId);
+
+    @PostMapping("/accept-form/{formId}")
+    public ResponseEntity<?> acceptForm(@PathVariable Long formId) {
+        service.acceptForm(formId);
         return ResponseEntity.ok("Form accepted");
     }
-    
-    @PostMapping("/{id}/reject-form/{formId}")
-    public ResponseEntity<?> rejectForm(@PathVariable Long id, @PathVariable Long formId) {
-        professorService.rejectForm(id, formId);
+
+    @PostMapping("/reject-form/{formId}")
+    public ResponseEntity<?> rejectForm(@PathVariable Long formId) {
+        service.rejectForm(formId);
         return ResponseEntity.ok("Form rejected");
     }
-    
-    @PostMapping("/suggest-juries/{formId}")
-    public ResponseEntity<?> suggestJuries(
-        @PathVariable Long formId,
-        @RequestBody List<Long> juryIds
-    ) {
-        professorService.suggestJuries(formId, juryIds);
+
+    @GetMapping("/meetings")
+    public ResponseEntity<?> getMeetings() {
+        return ResponseEntity.ok(service.getMeetings());
+    }
+
+
+    @PostMapping("/suggest-juries")
+    public ResponseEntity<?> suggestJuries(@RequestBody FormSuggestionInputDTO input) {
+        service.suggestJuries(input);
         return ResponseEntity.ok("Juries suggested");
     }
-    
+
     @PostMapping("/give-time")
-    public ResponseEntity<?> specifyAvailableTime(
-        @RequestParam Long professorId,
-        @RequestBody TimeSlotDTO timeSlotDTO
-    ) {
-        professorService.specifyAvailableTime(professorId, timeSlotDTO);
+    public ResponseEntity<?> specifyAvailableTime(@RequestBody AvailableTimeInputDTO availableTimeInputDTo) {
+        service.specifyAvailableTime(availableTimeInputDTo);
         return ResponseEntity.ok("Time slot added");
     }
+
+    @PostMapping("/schedule-meeting/{meetingId}")
+    public ResponseEntity<?> scheduleMeeting(@PathVariable Long meetingId) {
+        service.scheduleMeeting(meetingId);
+        return ResponseEntity.ok("Meeting scheduled");
+    }
+
+    @PostMapping("/cancel-meeting/{meetingId}")
+    public ResponseEntity<?> cancelMeeting(@PathVariable Long meetingId) {
+        service.cancelMeeting(meetingId);
+        return ResponseEntity.ok("Meeting canceled");
+    }
+
+    @PostMapping("/complete-meeting")
+    public ResponseEntity<?> completeMeeting(@RequestBody MeetingCompletionInputDTO input) {
+        service.completeMeeting(input);
+        return ResponseEntity.ok("Meeting completed");
+    }
+
 }
--- src/main/java/ir/kghobad/thesis_defense_time_schedular/dao/ProfessorRepository.java
+++ src/main/java/ir/kghobad/thesis_defense_time_schedular/dao/ProfessorRepository.java
@@ -2,8 +2,11 @@ package ir.kghobad.thesis_defense_time_schedular.dao;
 
 import ir.kghobad.thesis_defense_time_schedular.model.entity.user.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+    @Query("SELECT p.manager FROM Professor p WHERE p.id = ?1")
+    boolean isManager(Long instructorId);
 }
